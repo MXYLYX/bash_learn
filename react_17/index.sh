a91c2180fae3a6b0348bcdee25156a6a58ea4af5
MXYLYX <88269845+MXYLYX@users.noreply.github.com>
2022-09-23 18:20:45 +0800

    测试 一个 react 脚手架自动build

--- react_17/index.sh
+++ react_17/index.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 cd ~/git_project/react_17
-if /usr/bin/git pull; then
-    npm run build
-else
-    echo "Not Ok"
+echo && git pull origin master >> ~/build.log
+if [ $? -eq 0 ]; then
+    echo 'git pull ok'
+    echo && npm run build > ~/build.log
+    if [ $? -eq 0 ]; then
+        echo 'build ok'
+    else 
+        echo 'build fail'
+    fi
+else 
+    echo 'git pull fail'
 fi
